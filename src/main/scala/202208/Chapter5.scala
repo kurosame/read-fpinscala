d3ec67f661759f76fb8724f4645f7755194c208c
kurosame <kurosame1227@gmail.com>
2022-11-07 18:59:22 +0900

    fix

--- src/main/scala/202208/Chapter5.scala
+++ src/main/scala/202208/Chapter5.scala
@@ -543,6 +543,72 @@ object Chapter5 {
   // }
   // 正解
 
+  /** EXERCISE 5.12
+    *
+    * unfoldを使ってfibs、from、constant、onesを記述せよ。
+    */
+  // case object Empty extends Stream[Nothing]
+  // case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
+
+  // trait Stream[+A] {
+  //   def take(n: Int): Stream[A] = (this, n) match {
+  //     case (Empty, _)      => Empty
+  //     case (_, 0)          => Empty
+  //     case (Cons(h, t), _) => Stream.cons(h(), t().take(n - 1))
+  //   }
+
+  //   def toList: List[A] = this match {
+  //     case Empty      => Nil
+  //     case Cons(h, t) => h() :: t().toList
+  //   }
+
+  //   def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = f(z) match {
+  //     case None         => Empty
+  //     case Some((a, s)) => Stream.cons(a, unfold(s)(f))
+  //   }
+
+  //   // EXERCISE 5.12
+  //   def fibs: Stream[Int] = {
+  //     def go(x: Int, y: Int): Stream[Int] = ???
+  //     go(0, 1)
+  //   }
+
+  //   def from(n: Int): Stream[Int] = unfold(n)(x => Some(x, x + 1))
+
+  //   def constant[A](a: A): Stream[A] = unfold(a)(x => Some(x, x))
+
+  //   def ones: Stream[Int] = unfold(1)(x => Some(x, x))
+  // }
+
+  // object Stream {
+  //   def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
+  //     lazy val head = hd
+  //     lazy val tail = tl
+  //     Cons(() => head, () => tail)
+  //   }
+
+  //   def empty[A]: Stream[A] = Empty
+
+  //   def apply[A](as: A*): Stream[A] =
+  //     if (as.isEmpty) empty
+  //     else cons(as.head, apply(as.tail: _*))
+  // }
+
+  // def main(args: Array[String]): Unit = {
+  //   println(Empty.fibs.take(8).toList) // List(0, 1, 1, 2, 3, 5, 8, 13)
+  //   println(Empty.from(1).take(5).toList) // List(1, 2, 3, 4, 5)
+  //   println(Empty.constant(1).take(5).toList) // List(1, 1, 1, 1, 1)
+  //   println(Empty.constant('a').take(5).toList) // List(a, a, a, a, a)
+  //   println(Empty.ones.take(5).toList) // List(1, 1, 1, 1, 1)
+  // }
+  // fibsは不正解、正解は以下
+  // def fibs: Stream[Int] = unfold((0, 1))(x => Some(x._1, (x._2, x._1 + x._2)))
+  // fromは正解
+  // constantは正解だが、以下の方が良い
+  // def constant[A](a: A): Stream[A] = unfold(())(_ => Some(a, ()))
+  // onesは正解だが、以下の方が良い
+  // def ones: Stream[Int] = unfold(())(_ => Some(1, ()))
+
   /**
     */
 }
