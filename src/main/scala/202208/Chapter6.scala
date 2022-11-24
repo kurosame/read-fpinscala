d0e3fbd6b97477e84bc3e2916fc12d41bb9bd216
kurosame <kurosame1227@gmail.com>
2022-11-24 17:50:55 +0900

    fix

--- src/main/scala/202208/Chapter6.scala
+++ src/main/scala/202208/Chapter6.scala
@@ -95,6 +95,72 @@ object Chapter6 {
   //   (i / (Int.MaxValue.toDouble + 1), r)
   // }
 
+  /** EXERCISE 6.3
+    *
+    * ペア（Int,Double）、ペア（Double,Int）、および3要素のタプル（Double,Double,Double）を生成する関数を記述せよ。
+    * すでに作成済みの関数を再利用できるはずだ。
+    *
+    * def intDouble(rng: RNG): ((Int, Double), RNG)
+    * def doubleInt(rng: RNG): ((Double, Int), RNG)
+    * def double3(rng: RNG): ((Double, Double, Double), RNG)
+    */
+  // trait RNG {
+  //   def nextInt: (Int, RNG)
+  // }
+
+  // case class SimpleRNG(seed: Long) extends RNG {
+  //   def nextInt: (Int, RNG) = {
+  //     val newSeed = (seed * 0x5deece66dL + 0xbL) & 0xffffffffffffL
+  //     val nextRNG = SimpleRNG(newSeed)
+  //     val n = (newSeed >>> 16).toInt
+  //     (n, nextRNG)
+  //   }
+
+  //   def nonNegativeInt(rng: RNG): (Int, RNG) = {
+  //     val (i, r) = rng.nextInt
+  //     (if (i < 0) Math.abs(i + 1) else i, r)
+  //   }
+
+  //   def double(rng: RNG): (Double, RNG) = {
+  //     val (i, r) = nonNegativeInt(rng)
+  //     (i / (Int.MaxValue.toDouble + 1), r)
+  //   }
+
+  //   // EXERCISE 6.3
+  //   def intDouble(rng: RNG): ((Int, Double), RNG) = {
+  //     val (i, r) = nonNegativeInt(rng)
+  //     ((i, i.toDouble), r)
+  //   }
+
+  //   def doubleInt(rng: RNG): ((Double, Int), RNG) = {
+  //     val (i, r) = nonNegativeInt(rng)
+  //     ((i.toDouble, i), r)
+  //   }
+
+  //   def double3(rng: RNG): ((Double, Double, Double), RNG) = {
+  //     val (i1, r1) = nonNegativeInt(rng)
+  //     val (i2, r2) = nonNegativeInt(r1)
+  //     val (i3, r3) = nonNegativeInt(r2)
+  //     ((i1.toDouble, i2.toDouble, i3.toDouble), r3)
+  //   }
+  // }
+
+  // def main(args: Array[String]): Unit = {
+  //   val rng = SimpleRNG(1)
+  //   val (n1, rng2) = rng.intDouble(rng)
+  //   println(n1) // (384748,384748.0)
+  //   println(rng2) // SimpleRNG(25214903928)
+  //   val rng3 = SimpleRNG(-2)
+  //   val (n2, rng4) = rng3.doubleInt(rng3)
+  //   println(n2) // (769497.0,769497)
+  //   println(rng4) // SimpleRNG(281424546902833)
+  //   val rng5 = SimpleRNG(2)
+  //   val (n3, rng6) = rng5.double3(rng5)
+  //   println(n3) // (769497.0,1.988230381E9,1.277571484E9)
+  //   println(rng6) // SimpleRNG(197748051927719)
+  // }
+  // 正解（模範とは微妙に違うけど）
+
   /**
     */
 }
